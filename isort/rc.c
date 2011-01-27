0eb243e04c6dd227d42d088b15e0f49a5b8cd0e7
Tay Ray Chuan <rctay89@gmail.com>
2011-01-27 22:14:29 +0800

    add read loop

---
+++ isort/rc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int read_loop(int *input) {
+	int i = 0,
+		read;
+
+	scanf("%d", &read);
+	*size = i = read;
+
+	input = malloc(read * sizeof(int));
+	if (!input) {
+		fprintf(stderr, "malloc failed\n");
+		return -1;
+	}
+
+	while (i && scanf("%d", &read) == 1) {
+		switch (read) {
+		case '\r':
+		case '\n':
+			continue;
+		default:
+			*input = read;
+			input++;
+			i--;
+		}
+	}
+
+	return size;
+}
+
+int main() {
+	int *input, size;
+	int ret = 0;
+	if ((ret = read_loop(input)) < 0) {
+		fprintf(stderr, "read loop failed\n");
+		return ret;
+	}
+
+	size = ret;
+	return 0;
+}
